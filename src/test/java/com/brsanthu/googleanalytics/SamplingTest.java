6e8cfa9f379179b55e9183cb7a9984521dfd46f7
Mike Hardy <github@mikehardy.net>
2019-06-27 12:51:47 -0500

    lint: remove unused member and associated imports

--- src/test/java/com/brsanthu/googleanalytics/SamplingTest.java
+++ src/test/java/com/brsanthu/googleanalytics/SamplingTest.java
@@ -7,16 +7,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.brsanthu.googleanalytics.httpclient.HttpClient;
 import com.brsanthu.googleanalytics.internal.GoogleAnalyticsImpl;
 import com.brsanthu.googleanalytics.request.ScreenViewHit;
 
 public class SamplingTest {
-    private static final Logger logger = LoggerFactory.getLogger(SamplingTest.class);
-    
     private HttpClient mockHttpClient = null;
     
     @Before
